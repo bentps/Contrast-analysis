511ce057f45f3f820fbda60edab199bf8235edbb
bentps <ben@benps.com>
2017-08-13 12:18:46 -0700

    Upload data object
    The main method of Data_object is load, which imports contrast signal and bragg monitor CSV files, extracts the data, and creates various lists and dictionaries. Data objects are then further processed by the Phase_object and the Bragg_object.

---
+++ Data_object.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 30 12:11:50 2017
+
+@author: benps
+"""
+
+import pandas as pd
+import numpy as np
+
+#changed Time dict to signals dict
+
+class Data:
+    def __init__(self, date, N):
+        self.date=date
+        self.N=N
+        self.signals={}#sets of signals for a given time labled by time twoT.
+        self.avg_signal={}
+        self.time_list=[]
+        self.bragg_dict={}
+        self.avg_bragg_dict={}
+#this is the standard CSV import function, returning a numpy array of the oscilliscope voltage (signal) and time (timebase).
+    def csv(self, filename):
+        the_data = pd.read_csv(filename, names=['a','b','c','d','e'])
+        timebase=[i for i in the_data.c]
+        timebase=np.array(timebase)
+        signal=[i for i in the_data.d]
+        signal=np.array(signal)
+        return timebase, signal
+
+    def load(self, twoT, first, last, skiplist, letter):
+        date=self.date
+        self.time_list.append(twoT) #
+        if twoT in self.signals.keys():
+            datapoints=self.signals[twoT]
+        else:
+            datapoints=[]
+        if twoT in self.bragg_dict.keys():
+            braggpoints=self.bragg_dict[twoT]
+        else:
+            braggpoints=[]
+            
+        if twoT in self.avg_signal.keys():
+            sumdata=self.avg_signal[twoT][1]*(last-first-len(skiplist))
+        else:
+            sumdata=[0]*2500
+        if twoT in self.avg_bragg_dict.keys():
+            sumbragg1=self.avg_bragg_dict[twoT][1]*(last-first-len(skiplist))
+            sumbragg2=self.avg_bragg_dict[twoT][2]*(last-first-len(skiplist))
+        else:
+            sumbragg1=[0]*2500
+            sumbragg2=[0]*2500         
+        for i in range(first,last):
+            if i not in skiplist:
+                if i<10:
+                    filename=date+letter+'/signal/ALL000'+str(i)+'/F000'+str(i)+'CH1.CSV'                        
+                    signal_tuple=self.csv(filename)
+                    datapoints.append(signal_tuple)
+                    sumdata=sumdata+signal_tuple[1]  
+                    
+                    bragg1file=date+letter+'/bragg/ALL000{}/F000{}CH{}.CSV'.format(i,i,1)
+                    bragg2file=date+letter+'/bragg/ALL000{}/F000{}CH{}.CSV'.format(i,i,2)
+                    bragg1_tuple=self.csv(bragg1file)
+                    bragg2_tuple=self.csv(bragg2file)
+                    braggpoints.append((bragg1_tuple,bragg2_tuple))
+                    sumbragg1=sumbragg1+bragg1_tuple[1]
+                    sumbragg2=sumbragg2+bragg2_tuple[1]
+
+                else:
+                    filename=date+letter+'/signal/ALL00'+str(i)+'/F00'+str(i)+'CH1.CSV'                  
+                    signal_tuple=self.csv(filename)
+                    datapoints.append(signal_tuple)
+                    sumdata=sumdata+signal_tuple[1] 
+                    
+                    bragg1file=date+letter+'/bragg/ALL00{}/F00{}CH{}.CSV'.format(i,i,1)
+                    bragg2file=date+letter+'/bragg/ALL00{}/F00{}CH{}.CSV'.format(i,i,2)
+                    bragg1_tuple=self.csv(bragg1file)
+                    bragg2_tuple=self.csv(bragg2file)
+                    braggpoints.append((bragg1_tuple, bragg2_tuple))
+                    sumbragg1=sumbragg1+bragg1_tuple[1]
+                    sumbragg2=sumbragg2+bragg2_tuple[1]
+                    
+        #avg signal stuff    
+        timebase=signal_tuple[0]            
+        avg=sumdata/(last-first-len(skiplist)) 
+        self.avg_signal[twoT]=(timebase, avg)
+        #signal stuff
+        self.signals[twoT]=datapoints
+        #avg bragg stuff
+        braggbase=bragg1_tuple[0]    
+        avg_bragg1=sumbragg1/(last-first-len(skiplist))
+        avg_bragg2=sumbragg2/(last-first-len(skiplist))
+        self.avg_bragg_dict[twoT]=(braggbase, avg_bragg1, avg_bragg2)
+        #bragg stuff
+        self.bragg_dict[twoT]=braggpoints
+                    
+                    
